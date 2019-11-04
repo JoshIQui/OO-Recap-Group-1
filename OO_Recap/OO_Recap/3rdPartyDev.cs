e848573e810294a693eda99e5e43b0b61c15fe37
Joshua Quinones <jiq4449@g.rit.edu>
2019-11-04 16:32:04 -0500

    Added classes to use

---
+++ OO_Recap/OO_Recap/3rdPartyDev.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO_Recap
+{
+    class _3rd_Party_Dev
+    {
+    }
+}
